2979862b491eee243b2093b5a89873758f2a82fd
Ivan Khoda <ivankhoda01@gmail.com>
2023-09-27 20:15:55 +0300

    M.01. Change config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,8 +4,8 @@ require 'active_support/core_ext/integer/time'
 # test suite. You never need to work with it otherwise. Remember that
 # your test database is "scratch space" for the test suite and is wiped
 # and recreated between test runs. Don't rely on the data there!
-Rails.application.secrets.telegram[:bot][:token] = ENV['TELEGRAM'] unless Rails.application.secrets.telegram[:bot][:token]
 
+Telegram.bots_config = { default: ENV['TELEGRAM'] } unless Rails.application.secrets.telegram[:bot][:token]
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
