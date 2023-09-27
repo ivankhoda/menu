6363daa8609400cdfd9dc4efbcd9ff7d16996af5
Ivan Khoda <ivankhoda01@gmail.com>
2023-09-27 15:36:03 +0300

    M.01. Change add db.test config.

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe Dish, type: :model do
   subject { described_class.create(name: nil) }
   describe 'validations' do
     it 'should validate name' do
-      pp(subject.errors.size).to eq(2)
-
-      # expect(subject.errors[:name].size).to eq(1)
+      expect(subject.errors[:name].size).to eq(1)
     end
   end
 end
